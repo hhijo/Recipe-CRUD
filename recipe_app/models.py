b0f6257b013e350384b97eb7b7c6db5357c1a7ee
Wendy Atieno <136169879+hhijo@users.noreply.github.com>
2024-12-07 13:04:55 +0300

    Initial Commit

---
+++ recipe_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+# Recipe model
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.created_at}"
